0bb4cf405a1353ebd960135bd573462cbbcd1859
John <john.d.hutchinson@outlook.com>
2019-10-01 19:32:07 -0500

    assignment 4

---
+++ compst702/assignments/wordAvg.py
@@ -0,0 +1,14 @@
+def main():
+    sentence = input("Enter the sentence you would like analyzed: ")
+    sentList = []
+    sentList = sentence.split()
+    print(sentList)
+    counter = 0
+    total = 0
+    for i in sentList:
+        total = total + len(i)
+        counter += 1
+    average = total/counter
+    print("The average number of letters in each word in your sentence is:", average)
+    print("The total number of words is: ", counter)
+main()
