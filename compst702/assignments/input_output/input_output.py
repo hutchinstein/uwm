fac4b5bd979e76aaf246114f0bd0250aaecfd85c
John <john.d.hutchinson@outlook.com>
2019-10-10 19:58:55 -0500

    input_output for submission

--- compst702/assignments/input_output/input_output.py
+++ compst702/assignments/input_output/input_output.py
@@ -1,20 +1,22 @@
+# A file that opens one file, performs calculations and writes values #
+# to a new file.                                                      #
+# Calculations: square, cube, square root                             #
 import math
 
-read_file = input("Enter the file name containing your values: ")
-write_file = "PA5_output.txt"
+read_file = input("Enter the file name with your integers: ")
+write_file = input("Enter the output file name for: ")
 
 infile = open(read_file, "r")
 outfile = open(write_file, 'w')
 outputString = ''
-
 for line in infile:
     raw = int(line)
     square = int(line) ** 2
     cube = int(line) ** 3
     root = math.sqrt(int(line))
-
-    print(raw, square, cube, root, file=outfile)
-    print(raw, square, cube, root)
+    outputString = str(raw) + " " + str(square) + " " + str(cube) + " " + str(root) + "\n"
+    outfile.write(outputString)
+    print(outputString)
 
 infile.close()
 outfile.close()
