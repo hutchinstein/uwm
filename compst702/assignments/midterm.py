0199ab75cef36f2a70871e9f291e59a087df2d6b
John Hutchinson <john.d.hutchinson@outlook.com>
2019-11-02 15:43:18 -0500

    Added try/except

--- compst702/assignments/midterm.py
+++ compst702/assignments/midterm.py
@@ -22,25 +22,32 @@ def num_guess(game_number):
     hidden_number = random.randint(1, 100)
     total_games = 8
     guessed_numbers = []
+    valid_guess = False
     while not game_over:
-        user_guess = int(input("Guess a number between 1 and 100: "))
-        if user_guess > hidden_number:
-            guessed_numbers.append(user_guess)
-            print("Your guess is too high, try again!  You have " + str(total_games - (game_counter + 1)) +
-                  " guesses remaining")
-            print("You have guessed: ", guessed_numbers)
-        elif user_guess < hidden_number:
-            guessed_numbers.append(user_guess)
-            print("Your guess is too low, try again!  You have " + str(total_games - (game_counter + 1)) +
-                  " guesses remaining")
-            print("You have guessed: ", guessed_numbers)
-        else:
-            print("Good job, you guessed the number!")
-            game_over = True
-            victory = True
-        game_counter += 1
-        if game_counter > total_games - 1:
-            game_over = True
+        try:
+            user_guess = int(input("Guess a number between 1 and 100: "))
+            valid_guess = True
+        except ValueError:
+            print("Invalid selection.")
+        if valid_guess:
+            if user_guess > hidden_number:
+                guessed_numbers.append(user_guess)
+                print("Your guess is too high, try again!  You have " + str(total_games - (game_counter + 1)) +
+                      " guesses remaining")
+                print("You have guessed: ", guessed_numbers)
+            elif user_guess < hidden_number:
+                guessed_numbers.append(user_guess)
+                print("Your guess is too low, try again!  You have " + str(total_games - (game_counter + 1)) +
+                      " guesses remaining")
+                print("You have guessed: ", guessed_numbers)
+            else:
+                print("Good job, you guessed the number!")
+                game_over = True
+                victory = True
+            valid_guess = False
+            game_counter += 1
+            if game_counter > total_games - 1:
+                game_over = True
     if game_over and victory:
         outcome = "Victory"
     else:
