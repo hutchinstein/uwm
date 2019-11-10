4c5d8cdd708cc1ba31a05780b5c2428296b4a7d5
John <john.d.hutchinson@outlook.com>
2019-11-09 22:15:50 -0600

    mergesort

--- compst702/assignments/sandbox.py
+++ compst702/assignments/sandbox.py
@@ -1,4 +1,19 @@
-import random
+class foo :
+    def __init__(self, v):
+        self.value = v
+    def change(self,v):
+        value = v
+    def getValue(self):
+        return self.value
 
-print(random.random.__doc__)
-help(random)
+def main() :
+
+    x = foo(1)
+
+    x.change(10)
+
+    x.change(100)
+
+    print(x.getValue())
+
+main()
--- sandbox.py
+++
@@ -1,9 +0,0 @@
-import random
-
-num_list = []
-for i in range(0, 10001):
-    num_list.append(random.randint(0, 100000))
-
-
-num_list.sort()
-print(num_list)
