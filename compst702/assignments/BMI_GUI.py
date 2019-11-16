dc676b31af8be12e76c3d6850aff954de47d7486
John <john.d.hutchinson@outlook.com>
2019-11-15 19:52:57 -0600

    Finished BMI_GUI

--- compst702/assignments/BMI_GUI.py
+++ compst702/assignments/BMI_GUI.py
@@ -1,35 +1,9 @@
-# convert_gui.py
-# Program to convert Celsius to Fahrenheit using a simple
-#   graphical interface.
-"""Write a program that calculates BMI through a graphical user interface.
-User should be able to fill in height, with feet and inches separately in two entry boxes,
-and weight in pounds. It should display a button "Calculate". Only when the user clicks
-anywhere inside this button (ignoring all other clicks), the program should calculate
-and show BMI and the category (underweight/normal/overweight/obese). The category should
-be displayed in different colors (for example, normal in green, obese in red etc.). After
-that the "Calculate" button should turn into a "Quit" button. The program should close
-only when the user clicks anywhere inside this button.
-
-BMI = 703*weight/(height)**2
-
-where weight is in pounds and height is in inches.
-
-BMI Categories:
-Underweight: < 18.5
-Normal weight: 18.5–24.9
-Overweight:  25–29.9
-Obesity: >= 30
-
-Note: You may find it easier to begin with the convert_gui.py program and then slowly modifying it. I
-t may also help to first draw the interface on paper along with the coordinates."""
-
-
 from graphics import *
 
 
 def bmi_category(bmi):
     if bmi < 18.5:
-        color = "yellow"
+        color = "gold"
         category = "Underweight"
         return category, color
     elif 18.5 < bmi < 25.9:
@@ -53,64 +27,59 @@ def main():
     # Draw the interface
     Text(Point(1, 3.5), "Height (ft):").draw(win)
     heightFtInput = Entry(Point(2.1, 3.5), 5)
+    heightFtInput.draw(win)
 
     Text(Point(1, 3), "Height (in):").draw(win)
     heightInInput = Entry(Point(2.1, 3), 5)
+    heightInInput.draw(win)
 
     Text(Point(1, 2.5), "Weight (pounds):").draw(win)
     weightInput = Entry(Point(2.1, 2.5), 5)
+    weightInput.draw(win)
 
     Text(Point(1, 1), "BMI:").draw(win)
 
-    heightFtInput.draw(win)
-    heightInInput.draw(win)
-    weightInput.draw(win)
-
     output = Text(Point(2.1, 1), "")
     output.draw(win)
+
     button = Text(Point(1.5, 1.75), "Calculate")
     button.draw(win)
     rec = Rectangle(Point(1, 1.5), Point(2, 2))
     rec.draw(win)
 
-    # wait for a mouse click
-    clickPoint = win.getMouse()
-    clickPointY = clickPoint.getY()
-    clickPointX = clickPoint.getY()
-    # clickPointY, clickPointX = win.getMouse().getY(), win.getMouse().getX()
-    print("click!")
-    print(clickPointY, clickPointX)
-
-    # convert input
-    height = int(heightFtInput.getText()) * 12 + int(heightInInput.getText())
-    weight = int(weightInput.getText())
-    bmi = 703 * weight / height ** 2
-
-    result = bmi_category(bmi)[0]
-    fontColor = bmi_category(bmi)[1]
-    outString = str(bmi) + "\n" + result
+    # Enter loop to accept mouse clicks while BMI has not been calculated.
     gui_quit = False
-    print(gui_quit)
-    if not gui_quit:
-        if 1.5 <= clickPointY <= 2 and 1 <= clickPointX <= 2:
-            print("click1")
-            output.setText(str(outString))
-            output.setOutline(fontColor)
-            button.setText("Quit")
-            gui_quit = True
-            # clickPointX, clickPointY = 0, 0
-        else:
-            print("miss!")
+    while not gui_quit:
+        clickPoint = win.getMouse()
+        clickPointY = clickPoint.getY()
+        clickPointX = clickPoint.getX()
+        try:
+            height = int(heightFtInput.getText()) * 12 + int(heightInInput.getText())
+            weight = int(weightInput.getText())
+            bmi = 703 * weight / height ** 2
+            result = bmi_category(bmi)[0]
+            fontColor = bmi_category(bmi)[1]
+            outString = str(bmi) + "\n" + result
+        except ValueError:
+            continue
 
-
-    clickPointY = win.getMouse().getY()
-    clickPointX = win.getMouse().getX()
-    print("click!")
-    print(clickPointY, clickPointX)
-    if gui_quit:
-        if 1.5 <= clickPointY <= 2 and 1 <= clickPointX <= 2:
-            print("click2")
-            win.getMouse()
-            win.close()
+        if not gui_quit:
+            if 1.5 <= clickPointY <= 2 and 1 <= clickPointX <= 2:
+                output.setText(str(outString))
+                output.setOutline(fontColor)
+                button.setText("Quit")
+                gui_quit = True
+            else:
+                continue
+    # Enter loop to allow clicks after BMI has been calculated.
+    # Users are now able to quit.
+    while gui_quit:
+        quitClickPoint = win.getMouse()
+        quitClickPointY = quitClickPoint.getY()
+        quitClickPointX = quitClickPoint.getX()
+        if gui_quit:
+            if 1.5 <= quitClickPointY <= 2 and 1 <= quitClickPointX <= 2:
+                win.close()
+                break
 
 main()
